140781965da500deffa87de964eac0105ad490c3
Krzysztof Płocharz <krzysztof@plocharz.info>
2022-12-11 17:28:18 +0100

    aoc 9 part 2 done

--- 2022/src/aoc_9.rs
+++ 2022/src/aoc_9.rs
@@ -22,12 +22,14 @@ fn calc_tail(cur_x: usize, cur_y: usize, tail_x: usize, tail_y: usize) -> (usize
     if diff_x.abs() >= 2 || diff_y.abs() >= 2 {
         new_tail_x = (tail_x as i32 + unit(diff_x)) as usize;
         new_tail_y = (tail_y as i32 + unit(diff_y)) as usize;
-        return (new_tail_x, new_tail_y);
-    } else {
-        return (new_tail_x, new_tail_y);
     }
+    return (new_tail_x, new_tail_y);
 }
 
+struct Pos {
+    pub x: usize,
+    pub y: usize,
+}
 
 fn main() {
     let mut cur_x = 0i32;
@@ -63,11 +65,13 @@ fn main() {
     dbg!(max_y);
 
     let mut state: Vec<Vec<bool>> = vec![vec![false; (max_x - min_x + 1) as usize]; (max_y - min_y + 1) as usize];
-    // let mut state = [[false; (max_y - min_y)]; (max_x - min_x)];
     let mut cur_x = -min_x as usize;
     let mut cur_y = -min_y as usize;
-    let mut tail_x = cur_x;
-    let mut tail_y = cur_y;
+    let mut tail: Vec<Pos> = vec![];
+
+    for i in 0..10 {
+        tail.push(Pos{x: cur_x, y: cur_y});
+    }
 
     state[cur_y][cur_x] = true;
     let mut touched = 1;
@@ -78,19 +82,27 @@ fn main() {
 
         for _ in 0..steps {
             match dir {
-                "R" => cur_x += 1,
-                "L" => cur_x -= 1,
-                "U" => cur_y -= 1,
-                "D" => cur_y += 1,
+                "R" => tail[0].x += 1,
+                "L" => tail[0].x -= 1,
+                "U" => tail[0].y -= 1,
+                "D" => tail[0].y += 1,
                 _ => panic!("Invalid direction")
             }
-            (tail_x, tail_y) = calc_tail(cur_x, cur_y, tail_x, tail_y);
-            // dbg!(dir, tail_x, tail_y);
-            if state[tail_y][tail_x] == false {
+            for i in 0..(tail.len() - 1) {
+                (tail[i+1].x, tail[i+1].y) = calc_tail(tail[i].x, tail[i].y, tail[i+1].x, tail[i+1].y);
+            }
+            let last = tail.last().expect("No last element");
+            if state[last.y][last.x] == false {
                 touched += 1;
-                state[tail_y][tail_x] = true;
+                state[last.y][last.x] = true;
             }
         }
     }
+    // for i in state {
+    //     for j in i {
+    //         print!("{}", if j { "X" } else { "." });
+    //     }
+    //     println!("");
+    // }
     dbg!(touched);
 }
